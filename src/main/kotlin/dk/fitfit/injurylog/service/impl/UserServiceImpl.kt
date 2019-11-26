d997dde8ff48390fdda9ff2b438cecfb8148644c
sj <sebastianthegreatful@gmail.com>
2019-11-26 19:50:30 +0700

    Remove faulty added code from when trying to solve lazy loading issue

--- src/main/kotlin/dk/fitfit/injurylog/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/dk/fitfit/injurylog/service/impl/UserServiceImpl.kt
@@ -4,16 +4,10 @@ import dk.fitfit.injurylog.domain.User
 import dk.fitfit.injurylog.repository.UserRepository
 import dk.fitfit.injurylog.service.UserService
 import javax.inject.Singleton
-import javax.transaction.Transactional
 
 @Singleton
-@Transactional
-open class UserServiceImpl(private val userRepository: UserRepository) : UserService {
-    override fun save(user: User): User {
-        val saved = userRepository.save(user)
-        saved.roles.size
-        return saved
-    }
+class UserServiceImpl(private val userRepository: UserRepository) : UserService {
+    override fun save(user: User): User = userRepository.save(user)
 
     override fun getByEmail(email: String): User = userRepository.findByEmail(email) ?: throw UserNotFoundException(email)
 
