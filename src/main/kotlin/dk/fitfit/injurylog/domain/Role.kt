1253dcce5727e2c017fa955f9a40a7c8a499dddb
sj <sebastianthegreatful@gmail.com>
2019-12-11 00:00:53 +0700

    Remove faulty added tab character

--- src/main/kotlin/dk/fitfit/injurylog/domain/Role.kt
+++ src/main/kotlin/dk/fitfit/injurylog/domain/Role.kt
@@ -5,7 +5,7 @@ import javax.persistence.*
 
 @Entity
 class Role(@Column(nullable = false, unique = true) val name: String,
-           @JsonIgnore 	@ManyToMany(mappedBy = "roles") val users: List<User> = listOf(),
+           @JsonIgnore @ManyToMany(mappedBy = "roles") val users: List<User> = listOf(),
            @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) val id: Long = 0) {
     companion object {
         const val ADMIN = "ROLE_ADMIN"
