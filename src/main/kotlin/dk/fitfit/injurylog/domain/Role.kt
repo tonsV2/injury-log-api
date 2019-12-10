0b30d7eae6fed966e428d767c9cd637ca28f1386
sj <sebastianthegreatful@gmail.com>
2019-12-11 00:28:21 +0700

    Remove Role.users for two reasons 1. Fetching all users when fetching a role is a bad idea. Currently it's not a problem but if a role is added and given to a large amount of users it will cause the role object to be unnecessary large 2. Since it was annotated with @JsonIgnore it caused problems when deserializing it

--- src/main/kotlin/dk/fitfit/injurylog/domain/Role.kt
+++ src/main/kotlin/dk/fitfit/injurylog/domain/Role.kt
@@ -1,11 +1,9 @@
 package dk.fitfit.injurylog.domain
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 
 @Entity
 class Role(@Column(nullable = false, unique = true) val name: String,
-           @JsonIgnore @ManyToMany(mappedBy = "roles") val users: List<User> = listOf(),
            @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) val id: Long = 0) {
     companion object {
         const val ADMIN = "ROLE_ADMIN"
