bc2d05f0b6fda95684b3a122c82a54914f7553f7
sj <sebastianthegreatful@gmail.com>
2020-01-04 23:59:20 +0700

    Add test to ensure multiple image references result in multiple calls to FileStorageService.delete(key) with correct arguments
    Signed-off-by: sj <sebastianthegreatful@gmail.com>

--- src/test/kotlin/dk/fitfit/injurylog/repository/InjuryDeleteInterceptorTest.kt
+++ src/test/kotlin/dk/fitfit/injurylog/repository/InjuryDeleteInterceptorTest.kt
@@ -8,6 +8,7 @@ import io.micronaut.aop.MethodInvocationContext
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.mockk.verifySequence
 import org.junit.jupiter.api.Test
 
 internal class InjuryDeleteInterceptorTest {
@@ -34,6 +35,36 @@ internal class InjuryDeleteInterceptorTest {
 
         // Then
         verify(exactly = 1) { fileStorageService.delete(key) }
+        verify(exactly = 1) { methodInvocationContext.proceed() }
+    }
+
+    @Test
+    fun `Ensure interceptor invokes delete method with correct key with multiple image references`() {
+        // Given
+        val key = "id:filename"
+        val id = 123L
+        val imageReference = ImageReference(key, id)
+
+        val key2 = "id:filename2"
+        val id2 = 321L
+        val imageReference2 = ImageReference(key2, id2)
+
+        val injury = Injury(description = "", user = User(""), imageReferences = mutableListOf(imageReference, imageReference2))
+
+        every { methodInvocationContext.parameterValues } returns arrayOf(injury)
+        every { methodInvocationContext.proceed() } returns true
+        every { fileStorageService.delete(key) } returns Unit
+        every { fileStorageService.delete(key2) } returns Unit
+
+        // When
+        injuryDeleteInterceptor.intercept(methodInvocationContext)
+
+        // Then
+        verifySequence {
+            fileStorageService.delete(key)
+            fileStorageService.delete(key2)
+        }
+
         verify(exactly = 1) { methodInvocationContext.proceed() }
     }
 }
