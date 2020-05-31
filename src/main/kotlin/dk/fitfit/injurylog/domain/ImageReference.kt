2219a64b6c6313a01999fb1ddd7bbaeefc8286a8
sj <sebastianthegreatful@gmail.com>
2020-05-31 12:11:28 +0700

    Let ImageReference implement BaseEntity so it's updatable etc.

--- src/main/kotlin/dk/fitfit/injurylog/domain/ImageReference.kt
+++ src/main/kotlin/dk/fitfit/injurylog/domain/ImageReference.kt
@@ -1,12 +1,10 @@
 package dk.fitfit.injurylog.domain
 
+import dk.fitfit.injurylog.domain.core.BaseEntity
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 
 @Entity
 class ImageReference(
         val key: String,
-        @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) val id: Long = 0
-)
+        id: Long = 0
+) : BaseEntity(id)
